0dff6f5c8ce459c5f84cb999ba35c5238ddb991a
stevo-men <stevemenard3@gmail.com>
2023-10-02 09:21:09 -0400

    Refactoring : start method is final

--- src/Doctrina/Game.java
+++ src/Doctrina/Game.java
@@ -20,7 +20,7 @@ public abstract class Game {
         renderingEngine = new RenderingEngine();
     }
 
-    public void start() {
+    public final void start() {
         initiliaze();
         run();
 
