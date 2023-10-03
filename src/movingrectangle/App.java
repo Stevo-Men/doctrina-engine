0146ce3aa517f67376ecb608cc5461501c7bdb6a
stevo-men <stevemenard3@gmail.com>
2023-10-03 13:33:19 -0400

    Amazing new features

--- src/movingrectangle/App.java
+++ src/movingrectangle/App.java
@@ -1,4 +1,8 @@
 package movingrectangle;
 
 public class App {
+    public static void main(String[] args) {
+        (new movingRectangleGame()).start();
+    }
 }
+
--- src/movingrectangle/movingRectangleGame.java
+++ src/movingrectangle/movingRectangleGame.java
@@ -3,19 +3,29 @@ package movingrectangle;
 import Doctrina.Canvas;
 import Doctrina.Game;
 
+import java.awt.*;
+
 public class movingRectangleGame extends Game {
+
+    private Player player;
+    private NPC npc;
+
     @Override
     protected void initiliaze() {
-
+    player = new Player();
+    npc = new NPC();
     }
 
     @Override
     protected void update() {
-
+    player.update();
+    npc.update();
     }
 
     @Override
     protected void draw(Canvas canvas) {
-
+        canvas.drawRectangle(0,0,800,600, Color.BLUE);
+        player.draw(canvas);
+        npc.draw(canvas);
     }
 }
