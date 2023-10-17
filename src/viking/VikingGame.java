0fbed5be0766cee279e0512ec56859f40468385a
stevo-men <stevemenard3@gmail.com>
2023-10-17 14:57:25 -0400

    Feature : Tree

--- src/viking/VikingGame.java
+++ src/viking/VikingGame.java
@@ -8,27 +8,37 @@ public class VikingGame extends Game {
     private Player player;
     private GamePad gamePad;
     private World world;
+    private Tree tree;
 
     @Override
     protected void initialize() {
         gamePad = new GamePad();
         player = new Player(gamePad);
-        player.teleport(200,200);
+        player.teleport(200, 200);
         world = new World();
         world.load();
+        tree = new Tree();
+        tree.teleport(300, 350);
     }
 
     @Override
     protected void update() {
         if (gamePad.isQuitPressed()) {
             stop();
-            }
+        }
         player.update();
     }
 
     @Override
     protected void draw(Canvas canvas) {
         world.draw(canvas);
-        player.draw(canvas);
+        // 80 (tree height) - 28 (max for layer switch)
+        if (player.getY() < tree.getY() + 52) {
+            player.draw(canvas);
+            tree.draw(canvas);
+        } else {
+            tree.draw(canvas);
+            player.draw(canvas);
+        }
     }
 }
