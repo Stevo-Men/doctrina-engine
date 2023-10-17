9eb43743eeb2b25b9212afbe8a2ec6e43d067ee9
stevo-men <stevemenard3@gmail.com>
2023-10-17 14:39:53 -0400

    Feature : Viking Moving Animation

--- src/viking/Player.java
+++ src/viking/Player.java
@@ -2,26 +2,97 @@ package viking;
 
 import doctrina.Canvas;
 import doctrina.ControllableEntity;
+import doctrina.Direction;
 import doctrina.MovementController;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends ControllableEntity {
+
+    private static final String SPRITE_PATH = "images/player.png";
+    private static final int ANIMATION_SPEED =8;
+
+
+    private BufferedImage spriteSheet;
+    private Image[] rightFrames;
+    private Image[] leftFrames;
+    private Image[] upFrames;
+    private Image[] downFrames;
+    private int currentAnimationFrame = 1;
+    private int nextFrame = ANIMATION_SPEED;
 
     public Player(MovementController controller) {
         super(controller);
         setDimension(32, 32);
         setSpeed(3);
+        load();
     }
 
     @Override
     public void update() {
         super.update();
         moveWithController();
+        if (hasMoved()) {
+            --nextFrame;
+            if (nextFrame == 0) {
+                ++currentAnimationFrame;
+                if (currentAnimationFrame >= leftFrames.length) {
+                    currentAnimationFrame = 0;
+                }
+                nextFrame = ANIMATION_SPEED;
+            }
+        } else {currentAnimationFrame = 1;
+        }
     }
 
     @Override
     public void draw(Canvas canvas) {
-        canvas.drawRectangle(this, Color.RED);
+        if (getDirection() == Direction.RIGHT) {
+            canvas.drawImage(rightFrames[currentAnimationFrame], x, y);
+        } else if (getDirection() == Direction.LEFT) {
+            canvas.drawImage(leftFrames[currentAnimationFrame], x, y);
+        } else if (getDirection() == Direction.UP) {
+            canvas.drawImage(upFrames[currentAnimationFrame], x, y);
+        } else if (getDirection() == Direction.DOWN) {
+            canvas.drawImage(downFrames[currentAnimationFrame], x, y);
+        }
+    }
+
+    private void load() {
+        loadSpriteSheet();
+        loadAnimationFrames();
+    }
+
+    private void loadSpriteSheet() {
+        try {
+            spriteSheet = ImageIO.read(this.getClass().getClassLoader().getResourceAsStream(SPRITE_PATH));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void loadAnimationFrames() {
+        downFrames = new Image[3];
+        downFrames[0] = spriteSheet.getSubimage(0, 128, width, height);
+        downFrames[1] = spriteSheet.getSubimage(32, 128, width, height);
+        downFrames[2] = spriteSheet.getSubimage(64, 128, width, height);
+
+        leftFrames = new Image[3];
+        leftFrames[0] = spriteSheet.getSubimage(0, 160, width, height);
+        leftFrames[1] = spriteSheet.getSubimage(32, 160, width, height);
+        leftFrames[2] = spriteSheet.getSubimage(64, 160, width, height);
+
+        rightFrames = new Image[3];
+        rightFrames[0] = spriteSheet.getSubimage(0, 192, width, height);
+        rightFrames[1] = spriteSheet.getSubimage(32, 192, width, height);
+        rightFrames[2] = spriteSheet.getSubimage(64, 192, width, height);
+
+        upFrames = new Image[3];
+        upFrames[0] = spriteSheet.getSubimage(0, 224, width, height);
+        upFrames[1] = spriteSheet.getSubimage(32, 224, width, height);
+        upFrames[2] = spriteSheet.getSubimage(64, 224, width, height);
     }
 }
--- src/viking/VikingGame.java
+++ src/viking/VikingGame.java
@@ -7,24 +7,28 @@ public class VikingGame extends Game {
 
     private Player player;
     private GamePad gamePad;
+    private World world;
 
     @Override
     protected void initialize() {
         gamePad = new GamePad();
         player = new Player(gamePad);
         player.teleport(200,200);
+        world = new World();
+        world.load();
     }
 
     @Override
     protected void update() {
-if (gamePad.isQuitPressed()) {
-    stop();
-}
-player.update();
+        if (gamePad.isQuitPressed()) {
+            stop();
+            }
+        player.update();
     }
 
     @Override
     protected void draw(Canvas canvas) {
-
+        world.draw(canvas);
+        player.draw(canvas);
     }
 }
